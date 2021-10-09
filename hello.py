a7fb8096c263439c218a6db2200411d9b928cd70
danidoliv66 <92061732+danidoliv66@users.noreply.github.com>
2021-10-09 12:46:58 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,17 +1,17 @@
 def check(func):
   
-  def inner(str):
-    if len(str) > 20:
+  def inner(strg):
+    if len(strg) > 20:
       return "You said a lot! I can't handle it."
-    elif len(str.split()) > 10:
+    elif len(strg.split()) > 10:
       return "Too many words! Make a brief."
     else:
-      return func(str)
+      return func(strg)
   return inner
 
 @check()
-def say(str):
-  return str
+def say(strg):
+  return strg
 
 if __name__ == '__main__':
   say(input("Do you wanna say something?\n "))
